230ec2586d6df710d351b2373a184cd7863e5420
mjfalahee <mjfalahee@gmail.com>
2023-07-26 12:04:00 -0700

    switched to hashrouter for gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-const router = createBrowserRouter([
+const HashRouter = createHashRouter([
   {
-    path: "/projectpage/*",
+    path: "/*",
     element: <App />,
-  },
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={HashRouter} />
   </React.StrictMode>
 );
